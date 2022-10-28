ed15b1b544bcb921def09009a1e5f19a4a074524
LGpicinin <lgpicinin@gmail.com>
2022-10-28 18:07:56 -0300

    Mais e mais testes

--- vaiDar2.cpp
+++ vaiDar2.cpp
@@ -1,8 +1,12 @@
-include <iostream>
+#include <iostream>
 using std::cout;
 using std::endl;
 
 int main()
 {
     cout << "FUNCIONOU!!!!" << endl;
+
+    cout << "UHHHHHHH" << endl;
+
+    return 0;
 }
